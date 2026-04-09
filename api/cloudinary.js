8fdd477dbaf05484d50bec9199b12e5e8c202533
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2026-04-09 12:34:34 +0700

    Refactor Cloudinary API request handling

--- api/cloudinary.js
+++ api/cloudinary.js
@@ -1,18 +1,23 @@
 export default async function handler(req, res) {
   const cloudName = "dzbpzdqao";
   const apiKey = "978144777229154";
-  const apiSecret = "kb5h-WryZaiBzR7g3qulAF45iTo"; // ganti ini
+  const apiSecret = "kb5h-WryZaiBzR7g3qulAF45iTo"; // nanti lu amanin
+
+  const { max_results, cursor } = req.query;
 
   const auth = Buffer.from(apiKey + ":" + apiSecret).toString("base64");
 
-  const response = await fetch(
-    `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`,
-    {
-      headers: {
-        Authorization: `Basic ${auth}`,
-      },
-    }
-  );
+  let url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image?max_results=${max_results || 50}`;
+
+  if (cursor) {
+    url += `&next_cursor=${cursor}`;
+  }
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
 
   const data = await response.json();
 

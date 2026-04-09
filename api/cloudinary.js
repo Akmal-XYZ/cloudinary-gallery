91ad825a63b335e2a39532ff3392451804098cc1
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2026-04-09 13:23:22 +0700

    Update cloudinary.js

--- api/cloudinary.js
+++ api/cloudinary.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
-  const cloudName = "dzbpzdqao";
-  const apiKey = "978144777229154";
-  const apiSecret = "kb5h-WryZaiBzR7g3qulAF45iTo"; // nanti lu amanin
+  const cloudName = process.env.dzbpzdqao;
+  const apiKey = process.env.978144777229154;
+  const apiSecret = process.env.kb5h-WryZaiBzR7g3qulAF45iTo;
 
   const { max_results, cursor } = req.query;
 
   const auth = Buffer.from(apiKey + ":" + apiSecret).toString("base64");
 
-  let url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image?max_results=${max_results || 50}`;
+  let url = `https://api.cloudinary.com/v1_1/${cloudName}/resources?resource_type=all&max_results=${max_results || 50}`;
 
   if (cursor) {
     url += `&next_cursor=${cursor}`;
